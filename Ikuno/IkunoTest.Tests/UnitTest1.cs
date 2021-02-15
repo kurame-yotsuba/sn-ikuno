a963d36b971c93e7ed10f621db021ba7be431f0a
kurame <kuramechan428@gmail.com>
2021-02-15 20:00:05 +0900

    プロジェクトの作成

---
+++ Ikuno/IkunoTest.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IkunoTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
