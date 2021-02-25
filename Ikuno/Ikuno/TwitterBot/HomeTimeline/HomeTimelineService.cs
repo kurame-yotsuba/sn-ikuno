70e8e4c76e4c51c7bf6f9d5e35d88e1f4af1b66f
kurame <kuramechan428@gmail.com>
2021-02-26 00:17:24 +0900

    分けてたプロジェクトを一本化

---
+++ Ikuno/Ikuno/TwitterBot/HomeTimeline/HomeTimelineService.cs
@@ -0,0 +1,34 @@
+﻿using CoreTweet;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SwallowNest.Ikuno.TweetBot.HomeTimeline.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SwallowNest.Ikuno.TweetBot
+{
+    public class HomeTimelineService : BackgroundService
+    {
+        readonly ILogger<HomeTimelineService> _logger;
+        readonly TimeSpan _interval;
+
+        public HomeTimelineService(ILogger<HomeTimelineService> logger)
+        {
+            _logger = logger;
+            _interval = TimeSpan.FromSeconds(3);
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(stoppingToken.IsCancellationRequested is false)
+            {
+                _logger.LogInformation("タイムライン取得");
+                _tokens.Statuses.HomeTimeline()
+                await Task.Delay(_interval);
+            }
+        }
+    }
+}
