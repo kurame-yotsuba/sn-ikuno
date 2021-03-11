2ad6b233626e96c7b74227c239a96b554df97e13
kurame <kuramechan428@gmail.com>
2021-03-11 23:42:00 +0900

    internal クラスのテストをできるように属性を追加

--- Ikuno/Ikuno/Utility.cs
+++ Ikuno/Ikuno/Utility.cs
@@ -1,4 +1,10 @@
-﻿namespace SwallowNest.Ikuno
+﻿using System.Runtime.CompilerServices;
+
+#if DEBUG
+[assembly: InternalsVisibleTo("IkunoTest.Tests")]
+#endif
+
+namespace SwallowNest.Ikuno
 {
     internal static class Utility
     {
